026453257f34148d2747a1b537f8b5f805013f20
Kirin Tang <ktang91@my.bcit.ca>
2021-10-26 12:40:49 -0700

    finished'

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,11 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyCLi6YkQaxPJbGHuraTcXHuPjankY4WaIg",
+  authDomain: "demo08-kirin.firebaseapp.com",
+  projectId: "demo08-kirin",
+  storageBucket: "demo08-kirin.appspot.com",
+  messagingSenderId: "698677620643",
+  appId: "1:698677620643:web:dd52b06bf9957afbb07416",
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+const app = firebase.initializeApp(firebaseConfig);
